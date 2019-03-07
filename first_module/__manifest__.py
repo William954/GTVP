330311edb9d4dc02d1c1eb1e3ee830b20f83d0ff
William <william.colin@xmarts.com>
2019-03-07 16:55:52 -0600

    .

--- first_module/__manifest__.py
+++ first_module/__manifest__.py
@@ -1,34 +1,26 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Primer Modulo",
-
     'summary': """
         Creacion del primer modulo""",
-
     'description': """
        Este es el ejemplo de la creacion de un modulo desde 0
     """,
-
-    'author': "GTVP",
-    'website': "www.gtvp.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "Xmarts",
+    'website': "www.gtvp.odoo.com",    
+    'category': 'Modulo',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+
+    'installable': True,
+    'aplication': True,
+    'auto_install': False,
 }
--- first_module/models/models.py
+++ first_module/models/models.py
@@ -2,14 +2,25 @@
 
 from odoo import models, fields, api
 
-# class first_module(models.Model):
-#     _name = 'first_module.first_module'
+class VistaPrincipal(models.Model):
+	_name = 'autos'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+	name = fields.Char(string='Nombre del auto', help='Introduce el nombre del Vehiculo')
+	marca = fields.Many2one('autos.marca',string='Marca', help='Selecciona la marca del Vehiculo') # Este campo nos sirve para crear una relacion con otra tabla
+	modelo = fields.Char(string='Modelo')
+	year_fabricacion = fields.Date(string='Fecha de fabricacion')
+	color = fields.Char(string='Color')
+	kilomet = fields.Integer(string='Kilometraje')
+	garanty = fields.Boolean(string='Garantia')
+	transmision = fields.Selection([('1','Automatico'),('2','Surtido Rico'),('3','Estandar')],string='Transmision')
+	puertas = fields.Integer(string='No. de Puertas')
+	pasajeros = fields.Float(string='Pasajeros')
+	litros = fields.Float(string='Litros (motor)')
+	cilindros = fields.Integer(string='Cilindros')
+	combustible = fields.Selection([('1','Electricidad'),('2','Magna'),('3','Premium'),('4','Diesel')],string='Combustible')
+	picture = fields.Binary()
+
+class MarcasAutos(models.Model):
+	_name = 'autos.marca'
+
+	name = fields.Char(string='Nombre de la Marca')
